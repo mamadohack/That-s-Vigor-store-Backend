8ecae6a709c88230f9f78d49907efcade7105a2c
unknown <mamadohack@gmail.com>
2024-05-10 18:25:59 +0100

    backend of store

---
+++ src/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register({ strapi }) {
+    const extensionService = strapi.plugin('graphql').service('extension');
+    extensionService.use(({ nexus }) => ({
+      types: [
+        nexus.extendType({
+          type: 'UsersPermissionsMe',
+          definition(t) {
+            // here define fields you need
+            t.json('usercart');
+          },
+        }),
+      ]
+    }));
+  },
+
+  /**
+   * An asynchronous bootstrap function that runs before
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perform some special logic.
+   */
+  bootstrap({ strapi }) {},
+
+  /**
+   * An asynchronous destroy function that runs before
+   * your application gets shut down.
+   *
+   * This gives you an opportunity to gracefully stop services you run.
+   */
+  destroy({ strapi }) {},
+};
